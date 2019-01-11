8e62f8efa95140ced983af9848589f5c91a3df52
AhsanAzim <ahsan-azim@live.com>
2019-01-11 01:08:41 -0500

    flask port issue

--- api.py
+++ api.py
@@ -8,6 +8,10 @@ app.config["DEBUG"] = True
 
 print("RUNNING")
 
+@app.route('/')
+def hello_world():
+    return "hello"
+
 @app.route('/', methods=['GET', 'POST', 'OPTIONS'])
 @cross_origin()
 def content_extractor():
@@ -40,4 +44,7 @@ def content_extractor():
         response = jsonify(res_dict)
         return response
 
-app.run(host='0.0.0.0')
+# https://stackoverflow.com/questions/17260338/deploying-flask-with-heroku
+# Bind to PORT if defined, otherwise default to 5000.
+port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port)
